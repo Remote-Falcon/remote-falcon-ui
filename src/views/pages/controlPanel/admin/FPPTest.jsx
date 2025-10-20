2e2b07e8d31373901f16911f5d512fdc33efabf5
James Vance <whitesoup12@gmail.com>
2025-10-20 13:44:12 -0500

    FPP Test

--- src/views/pages/controlPanel/admin/FPPTest.jsx
+++ src/views/pages/controlPanel/admin/FPPTest.jsx
@@ -1,18 +1,28 @@
-
+import { useState } from 'react';
 import newAxios from 'axios';
-import { Grid } from '@mui/material';
+import { Grid, TextField } from '@mui/material';
 
 import MainCard from '../../../../ui-component/cards/MainCard';
 import RFLoadingButton from '../../../../ui-component/RFLoadingButton';
 
 const FPPTest = ({ }) => {
+
+  const [url, setUrl] = useState('');
+
   const getFPPVersion = async () => {
-    newAxios.get('http://192.168.68.156/api/fppd/version');
+    newAxios.get(url);
   };
 
   return (
     <Grid item xs={12}>
       <MainCard content={false}>
+        <TextField
+          type="text"
+          fullWidth
+          label="URL"
+          value={url}
+          onChange={(e) => setUrl(e?.target?.value)}
+        />
         <RFLoadingButton sx={{ ml: 3 }} color="error" onClick={() => getFPPVersion()}>
           Get Feedback
         </RFLoadingButton>
